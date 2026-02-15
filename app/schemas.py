18ded8a4249a819946f4934d812a750b6c3000c7
adabo4 <adabo4@gmail.com>
2026-02-15 14:17:08 +0100

    added vote on posts logic

--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,9 @@
-from typing import Optional
-from pydantic import BaseModel, EmailStr
+from typing import Optional, Annotated
+from pydantic import BaseModel, EmailStr, Field
 from datetime import datetime
+
+
+from pydantic.types import conint
 
 class PostBase(BaseModel):
     title: str
@@ -50,4 +53,7 @@ class Token(BaseModel):
 class TokenData(BaseModel):
     id: Optional[str] = None
     
+class Vote(BaseModel):
+    post_id: int
+    dir: Annotated[int, Field(ge=0, le=1)]  # dir can only be 0 or 1
 
